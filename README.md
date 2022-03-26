280c2e1ec67e4ac66a9c4a400d46af48dce379d6
Zhen-hui <cathytrinh123@gmail.com>
2022-03-26 22:15:02 +0000

    Provide more background info and instructions

--- README.md
+++ README.md
@@ -1,10 +1,19 @@
 # smarttrade
-This is a personal project: writing python functions to pick stocks based on defined selection criteria on a daily basis. Then run a simulation to trade the selected stocks and see whether or not the strategy work.
+In this project, we are writing python functions to pick stocks based on defined selection criteria on a daily basis. Then run a simulation to trade the selected stocks and see whether or not the strategy work.
 
-In order to run the program, we need to go to https://www.nasdaq.com/market-activity/stocks/screener and download the list of stocks listed in NASDAQ first.
 
-Current selection criteria are:    
+**Current selection criteria inlclude:**    
   1. Volume > 50000.     
   2. close t > close t-1.  
   3. RSI t-1 < 30 & RSI t > 30.  
   4. MACD t-1 < signal t-1 & MACD t > signal t.  
+
+
+**Prerequisites to run the program:**
+1. Download a list of NASDAQ stock symbols from https://www.nasdaq.com/market-activity/stocks/screener as 'nasdaq_screener.csv' to the project folder.
+2. Make sure all the required packages are installed by simply running the below command in your terminal.
+
+``` 
+pip3 install -r requirements.txt --user
+```
+
--- utils.py
+++ utils.py
@@ -12,73 +12,97 @@ warnings.filterwarnings('ignore')
 MIN_VOLUME = 50000
 
 def get_ticker_record(ticker):
-    #today = dt.datetime.today().strftime('%Y-%m-%d')
-    today = (dt.datetime.today() + dt.timedelta(days=1)).strftime('%Y-%m-%d')
+    """
+    This function returns a dataframe containing the 3-month history of a specific ticker using the yfinance package
+    """    
+    #today = dt.datetime.today().strftime('%Y-%m-%d') the data 
+    today = (dt.datetime.today() + dt.timedelta(days=1)).strftime('%Y-%m-%d') 
     previous_date = dt.datetime.today() - dateutil.relativedelta.relativedelta(months=3)
     previous_date = previous_date.strftime('%Y-%m-%d')
 
-    """
-    df = pdr.get_data_yahoo(ticker, previous_date, today)
-    """
     df = yf.Ticker(ticker).history(start=previous_date, end=today)
     return df
 
 
 def compute_rsi(ticker_record):
+    """
+    This function calculates the Relative Strength Index (RSI) of a ticker.
+    RSI is a momentum indicator that evaluates overbought or oversold conditions based on changes in stock price. 
+    RSI ranges between 0 and 100. Generally an RSI of 70 or more is considered as overbought/overvalued, and an RSI value of 30 or less is considered as oversold/undervalued.
+    
+    Formula:
+    RSI = 100 - 100/(1+RS)
+        * RS = average gain/average loss (2 steps in calculating average here)
+            * First average gain and average loss are simple 14-day averages
+                First Average Gain = Sum of Gains over the past 14 periods / 14
+                First Average Loss = Sum of Losses over the past 14 periods / 14
+            * The second, and subsequent, calculations are based on the prior averages and the current gain loss:
+                Average Gain = (previous Average Gain x 13 + current Gain) / 14
+                Average Loss = (previous Average Loss x 13 + current Loss) / 14
+    """    
+
+    # Initialize a dataframe to store the values
     df = ticker_record
     if not 'Adj Close' in df:
         df['Adj Close'] = df['Close']
-    ## Compute 14_Day RSI
-    df['Up Move'] = np.nan
-    df['Down Move'] = np.nan
-    df['Average Up'] = np.nan
-    df['Average Down'] = np.nan
-    # Relative Strength
-    df['RS'] = np.nan
-    # Relative Strength Index
-    df['RSI'] = np.nan
-    ## Calculate Up Move & Down Move
+    df['Daily Gain'] = np.nan 
+    df['Daily Loss'] = np.nan
+    df['Average Gain'] = np.nan 
+    df['Average Loss'] = np.nan
+    df['RS'] = np.nan  # Relative Strength
+    df['RSI'] = np.nan # Relative Strength Index
+   
+    ## Calculate Daily Gain & Daily Loss
     for x in range(1, len(df)):
-        df['Up Move'][x] = 0
-        df['Down Move'][x] = 0
-
+        df['Daily Gain'][x] = 0
+        df['Daily Loss'][x] = 0
+        
+        # If close price of date x is higher than that of date (x-1), then it's a gain, calculate the difference
         if df['Adj Close'][x] > df['Adj Close'][x-1]:
-            df['Up Move'][x] = df['Adj Close'][x] - df['Adj Close'][x-1]
-
+            df['Daily Gain'][x] = df['Adj Close'][x] - df['Adj Close'][x-1]
+        # If close price of date x is lower than that of date (x-1), then it's a loss, calculate the absolute difference
         if df['Adj Close'][x] < df['Adj Close'][x-1]:
-            df['Down Move'][x] = abs(df['Adj Close'][x] - df['Adj Close'][x-1])  
+            df['Daily Loss'][x] = abs(df['Adj Close'][x] - df['Adj Close'][x-1])  
 
-    ## Calculate initial Average Up & Down, RS and RSI
-    df['Average Up'][14] = df['Up Move'][1:15].mean()
-    df['Average Down'][14] = df['Down Move'][1:15].mean()
-    df['RS'][14] = df['Average Up'][14] / df['Average Down'][14]
+
+    ## Calculate initial Average Gain & Average Loss, RS, and RSI
+    df['Average Gain'][14] = df['Daily Gain'][1:15].mean()
+    df['Average Loss'][14] = df['Daily Loss'][1:15].mean()
+    df['RS'][14] = df['Average Gain'][14] / df['Average Loss'][14]
     df['RSI'][14] = 100 - (100/(1+df['RS'][14]))
-    ## Calculate rest of Average Up, Average Down, RS, RSI
+
+
+    ## Calculate rest of Average Gain, Average Loss, RS, and RSI
+    ## "Average Gain" and "Average Loss" values are smoothed after the first calculation
     for x in range(15, len(df)):
-        df['Average Up'][x] = (df['Average Up'][x-1]*13+df['Up Move'][x])/14
-        df['Average Down'][x] = (df['Average Down'][x-1]*13+df['Down Move'][x])/14
-        df['RS'][x] = df['Average Up'][x] / df['Average Down'][x]
+        df['Average Gain'][x] = (df['Average Gain'][x-1]*13+df['Daily Gain'][x])/14
+        df['Average Loss'][x] = (df['Average Loss'][x-1]*13+df['Daily Loss'][x])/14
+        df['RS'][x] = df['Average Gain'][x] / df['Average Loss'][x]
         df['RSI'][x] = 100 - (100/(1+df['RS'][x]))
+
+
     return df
 
 
 def compute_macd(ticker_record):
-    ## Calculate the MACD and Signal Line indicators
-    ## Calculate the Short Term Exponential Moving Average
+    """
+    This function calculates the following:
+        - Moving Average Convergence Divergence (MACD): the difference between 26-day and 12-day exponential moving averages (EMA)
+        - Signal: a 9-day EMA. A buy signal is triggered when MACD is above the signal, while a sell signal is triggered when MACD is below the signal.
+    """
 
     df = ticker_record
 
+    ## Calculate the Long Term Exponential Moving Average (12-day EMA)
     ShortEMA = df.Close.ewm(span=12, adjust=False).mean() 
 
-    ## Calculate the Long Term Exponential Moving Average
-
+    ## Calculate the Long Term Exponential Moving Average (26-day EMA)
     LongEMA = df.Close.ewm(span=26, adjust=False).mean() 
 
     ## Calculate the Moving Average Convergence/Divergence (MACD)
-
     MACD = ShortEMA - LongEMA
 
-    ## Calcualte the signal line
+    ## Calcualte the signal line (9-day EMA)
     signal = MACD.ewm(span=9, adjust=False).mean()
     
     df['MACD'] = MACD
@@ -86,12 +110,18 @@ def compute_macd(ticker_record):
     
     return df
 
+
 def get_single_ticker_info(ticker, filter_volume=True):
-    
+    """
+    This function returns current and previous days' close price, volume, RSI, MACD, and signal for a select ticker 
+    """
     try:
         ticker_record = get_ticker_record(ticker)
+
+        # If the colume of the ticker in both days is lower than our threshold, move to the next ticker without calculating any of the values
         if filter_volume  and ticker_record['Volume'][-1] < MIN_VOLUME:
             return None
+
         ticker_record = compute_rsi(ticker_record)
         ticker_record = compute_macd(ticker_record)
         ticker_record
@@ -99,7 +129,7 @@ def get_single_ticker_info(ticker, filter_volume=True):
         single_ticker_info = list()
         # ticker name
         single_ticker_info.append(ticker)
-        # today's close
+        # today's close price
         single_ticker_info.append(ticker_record['Close'][-1])
         # today's volume
         single_ticker_info.append(ticker_record['Volume'][-1])
@@ -127,12 +157,12 @@ def get_single_ticker_info(ticker, filter_volume=True):
         return None
 
 
+# Try on one ticker
 if __name__ == '__main__':
     ticker = 'RENT'
 
     ticker_info = get_ticker_record(ticker)
     print(ticker_info)
-    ticker_info = get_single_ticker_info(ticker)
-    #ticker_info = yf.Ticker(ticker).history(period='3mo')
 
+    ticker_info = get_single_ticker_info(ticker)
     print(ticker_info)
